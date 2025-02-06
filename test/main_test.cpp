e1ca219098339ed0be3faa26e2440e0fbb105a1e
Thijs Janzen <thijsjanzen@gmail.com>
2025-02-06 12:00:50 +0100

    add more tests

--- test/main_test.cpp
+++ test/main_test.cpp
@@ -52,6 +52,95 @@ TEST_CASE("test birth")
     CHECK(offspring.ID == id_counter);
 }
 
+TEST_CASE("test nursing")
+{
+    parameters params;
+    simulation sim(params);
+    
+    size_t id_counter = 0;
+    auto mother = individual(params.init_energy,
+                             life_stage::mother,
+                                 ++id_counter);
+    
+    auto pup = mother.reproduce(++id_counter, params.init_offspring_energy);
+    
+    mother.produce_milk(params);
+    
+    double pup_energy_before = pup.energy;
+    double mother_milk_before = mother.milk;
+    
+    sim.nurse(&pup, &mother, params);
+    
+    double mother_milk_after = mother.milk;
+    double pup_energy_after = pup.energy;
+    
+    CHECK(pup_energy_after > pup_energy_before);
+    CHECK(mother_milk_after < mother_milk_before);
+    CHECK(mother_milk_before - mother_milk_after == params.nurse_amount * params.milk_consumption);
+}
+
+TEST_CASE("test update mothers")
+{
+    parameters params;
+    params.c_survival_mother = 10000;
+    simulation sim(params);
+    sim.initialize();
+    
+    // with survival switched off, all mothers survive.
+    // also, in the initial time step, mothers have full energy and no incentive to forage
+    sim.update_mothers();
+    
+    CHECK(sim.mothers.size() == params.init_population_size);
+    CHECK(sim.available_mothers.size() == params.init_population_size);
+    
+    params.c_survival_mother = 0; // we kill all mothers
+    simulation sim2(params);
+    sim2.initialize();
+    sim2.update_mothers();
+    CHECK(sim2.mothers.size() == 0);
+    CHECK(sim2.available_mothers.size() == 0);
+}
+
+TEST_CASE("test update pups")
+{
+    parameters params;
+    params.c_survival_pup = 10000;
+    simulation sim(params);
+    sim.initialize();
+    sim.update_mothers();
+    
+    sim.update_pups();
+    CHECK(sim.pups.size() == params.init_population_size);
+    
+    params.c_survival_pup = 0;
+    simulation sim2(params);
+    sim2.initialize();
+    sim2.update_mothers();
+    
+    sim2.update_pups();
+    CHECK(sim2.pups.size() == 0);
+}
+
+TEST_CASE("test find mother") {
+    parameters params;
+    params.c_survival_pup = 10000;
+    params.c_survival_mother = 10000;
+    simulation sim(params);
+    sim.initialize();
+    sim.update_mothers();
+    
+    // all mothers and pups should be alive now
+    for (const auto& i : sim.pups) {
+        auto mother_index = sim.find_mother(i.mother_ID);
+        CHECK(mother_index >= 0);
+    }
+    
+    
+    
+    
+    
+}
+
 /*
 int main(int argc, const char * argv[]) {
     std::cout << "This is running!" << std::endl;
